76e864680a550eb6c1f4e4f9bebd9940e198db18
jessy413 <jessy413@ucla.edu>
2022-10-31 19:34:40 -0700

    first commit

---
+++ steering_test.ino
@@ -0,0 +1,67 @@
+// INPUTS//
+
+#define DAQ_BUTTON 14
+#define HEADLIGHT_SWITCH 15
+#define LEFT_TURN_SWITCH 16
+#define RIGHT_TURN_SWITCH 17
+#define HAZARD_SWTICH 18
+#define BRAKE_SWITCH 19 
+#define HORN_SWITCH 20
+#define WIPER_SWITCH 21
+
+// OUTPUTS//
+
+#define HEADLIGHT_MOSFET 0
+#define LEFT_TURN_MOSFETc 1
+#define RIGHT_TURN_MOSFET 2
+#define BRAKE_MOSFET 4
+#define HORN_MOSFET 5
+#define LED1 7
+#define LED2 8
+#define IN_1 9
+#define IN_2 10
+#define IN_3 11
+#define IN_4 12
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+ // INPUT FUNCTION// 
+  pinMode(DAQ_BUTTON, INPUT);
+  pinMode(HEADLIGHT_SWITCH, INPUT);
+  pinMode(LEFT_TURN_SWTICH, INPUT);
+  pinMode(RIGHT_TURN_SWTICH, INPUT);
+  pinMode(HAZARD_SWITCH, INPUT);
+  pinMode(BRAKE_SWTICH, INPUT);
+  pinMode(HORN_SWTICH, INPUT);
+  pinMode(WIPER_SWTICH, INPUT);
+
+
+  // OUTPUTS
+  pinMode(HEADLIGHT_MOSFET, OUTPUT);
+  pinMode(LEFT_TURN_MOSFET, OUTPUT);
+  pinMode(RIGHT_TURN_MOSFET, OUTPUT);
+  pinMode(BRAKE_MOSFET, OUTPUT);
+  pinMode(HORN_MOSFET, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT);
+ 
+}
+
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(HEADLIGHT_SWITCH)){
+    digitalWrite(HEADLIGHT_MOSFET, HIGH);
+    
+  } else {
+    digitalWrite(HEADLIGHT_MOSFET, LOW);
+  }
+}
