a84afc1d7659580289edff15ca8ce70a710a58ad
Cristobal Velazquez <cvelazquez581416@gmail.com>
2023-03-11 20:40:07 -0800

    Mostly working PCB code

--- steering_test/steering_test.ino
+++ steering_test/steering_test.ino
@@ -10,12 +10,11 @@
 #define BRAKE_SWITCH 19 
 #define HORN_SWITCH 20
 #define WIPER_SWITCH 21
+#define LIMIT_SWITCH 13
 
 // OUTPUTS //
 
 #define HEADLIGHT_MOSFET 0
-#define LEFT_TURN_MOSFET 1
-#define RIGHT_TURN_MOSFET 2
 #define BRAKE_MOSFET 4
 #define HORN_MOSFET 5
 #define LED1 7
@@ -61,6 +60,23 @@ struct LEDStateStrip {
 LEDStateStrip left_turn_leds;
 LEDStateStrip right_turn_leds;
 
+LEDStateStrip back_left_turn_leds;
+LEDStateStrip back_right_turn_leds;
+
+
+
+// --------------- WIPER CONTROL ----------------
+
+enum WIPER_STATE {EXTEND, RETRACT, EXTEND_DELAY, RETRACT_DELAY};
+
+struct WiperController {
+  const long unsigned int mosfet_delay = 2000;
+  long unsigned int current_time;
+  bool move_up;
+  WIPER_STATE state;
+};
+
+WiperController wiper_controller;
 
 // --------------- PRE-SET-UP ---------------
 
@@ -69,6 +85,11 @@ bool update_device(int port);
 
 void reset_leds(LEDStateStrip &strip);
 void set_leds_turn(LEDStateStrip &strip);
+
+
+void set_wiper_mosfets(String direction);
+void reset_wiper(WiperController &wiper);
+void set_wiper(WiperController &wiper);
 
 // ------------------------------------------------------------------------------ SETUP () --------------------------------------------------------------------
 void setup() {
@@ -85,12 +106,11 @@ void setup() {
   pinMode(BRAKE_SWITCH, INPUT);
   pinMode(HORN_SWITCH, INPUT);
   pinMode(WIPER_SWITCH, INPUT);
+  pinMode(LIMIT_SWITCH, INPUT);
 
 
   // OUTPUTS //
   pinMode(HEADLIGHT_MOSFET, OUTPUT);
-  pinMode(LEFT_TURN_MOSFET, OUTPUT);
-  pinMode(RIGHT_TURN_MOSFET, OUTPUT);
   pinMode(BRAKE_MOSFET, OUTPUT);
   pinMode(HORN_MOSFET, OUTPUT);
   pinMode(LED1, OUTPUT);
@@ -108,21 +128,52 @@ void setup() {
   right_turn_leds.leds_on = 0;
   right_turn_leds.leds_off = 0;
   
+  back_left_turn_leds.leds_on = 0;
+  back_left_turn_leds.leds_off = 0;
+  back_right_turn_leds.leds_on = 0;
+  back_right_turn_leds.leds_off = 0;
+  
+  
   left_turn_leds.state = ON;
   right_turn_leds.state = ON;
+  
+  back_left_turn_leds.state = ON;
+  back_right_turn_leds.state = ON;
+  
   
 
   // BRAKE LED SET-UP //
 
-  //FastLED.addLeds<WS2812, BRAKE_LED_PIN, GRB>(brake_leds, BRAKE_NUM_LEDS);
+  FastLED.addLeds<WS2812, BRAKE_LED_PIN, GRB>(brake_leds, BRAKE_NUM_LEDS);
 
   // TURN LED SET-UP //
 
   FastLED.addLeds<WS2812, FRONT_LEFT_TURN_LED_PIN, GRB>(left_turn_leds.leds, TURN_NUM_LEDS);
   FastLED.addLeds<WS2812, FRONT_RIGHT_TURN_LED_PIN, GRB>(right_turn_leds.leds, TURN_NUM_LEDS);
+  FastLED.addLeds<WS2812, BACK_LEFT_TURN_LED_PIN, GRB>(back_left_turn_leds.leds, TURN_NUM_LEDS);
+  FastLED.addLeds<WS2812, BACK_RIGHT_TURN_LED_PIN, GRB>(back_right_turn_leds.leds, TURN_NUM_LEDS);
   
+  // WIPER SET-UP //
+
+  wiper_controller.state = EXTEND;
+  wiper_controller.move_up = true;
+
+  // RESET WIPER MOSFETS
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, LOW);
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, LOW);
+  delay(50);
+
+  // SET TIMER VARS //
+  
+  left_turn_leds.current_time = millis();
   right_turn_leds.current_time = millis();
-  right_turn_leds.current_time = millis();
+  
+  back_left_turn_leds.current_time = millis();
+  back_right_turn_leds.current_time = millis();
+  
+  wiper_controller.current_time = millis();
 }
 
 // ------------------------------------------------------------------------ MAIN LOOP ---------------------------------------------------------------------------
@@ -142,34 +193,58 @@ void loop() {
   Serial.print("Horn Switch: ");
   Serial.println(read_switch(HORN_SWITCH));
 
-  delay(50);
   
   // SETTING TURN + HAZARD LIGHTS
   if(read_switch(HAZARD_SWITCH)){
     set_leds_turn(left_turn_leds);
     set_leds_turn(right_turn_leds);
+    set_leds_turn(back_left_turn_leds);
+    set_leds_turn(back_right_turn_leds);
   } 
   
   else if(read_switch(LEFT_TURN_SWITCH)){
     reset_leds(right_turn_leds);
     set_leds_turn(left_turn_leds);
+    reset_leds(back_right_turn_leds);
+    set_leds_turn(back_left_turn_leds);
   } 
   
   else if(read_switch(RIGHT_TURN_SWITCH)){
     reset_leds(left_turn_leds);
     set_leds_turn(right_turn_leds);
+    reset_leds(back_left_turn_leds);
+    set_leds_turn(back_right_turn_leds);
   } 
   
   else {
     reset_leds(left_turn_leds);
     reset_leds(right_turn_leds);
+    reset_leds(back_left_turn_leds);
+    reset_leds(back_right_turn_leds);
   }
+
+  // Remember to ask Achyuta what we're gonna do for the brake input to the board
+  // SETTING BRAKE LIGHTS
+  if(read_switch(BRAKE_SWITCH))
+    fill_solid(brake_leds, BRAKE_NUM_LEDS, CRGB(255, 0, 0)); 
+  else
+    fill_solid(brake_leds, BRAKE_NUM_LEDS, CRGB(0, 0, 0));
+
+  FastLED.show();
+
+  // SETTING WIPER
+  if(read_switch(WIPER_SWITCH))
+    set_wiper(wiper_controller);
+  else
+    reset_wiper(wiper_controller);
 
   // SETTING HEADLIGHTS
   update_device(HEADLIGHT_SWITCH, HEADLIGHT_MOSFET);
-
+  
   // SETTING HORN
   update_device(HORN_SWITCH, HORN_MOSFET);
+
+  delay(50);
 }
 
 // ------------------------------------------------------------------------ END OF MAIN LOOP ---------------------------------------------------------------------------
@@ -188,6 +263,7 @@ bool update_device(int input_port, int output_port){
   digitalWrite(output_port, state);
   return state;
 }
+
 
 
 // LED TURN STATE MACHINE
@@ -263,4 +339,80 @@ void reset_leds(LEDStateStrip &strip){
     FastLED.show();
     
     strip.current_time = millis();
+}
+
+
+
+
+// WIPER STATE MACHINE
+
+void set_wiper(WiperController &wiper){
+  if(wiper.state == EXTEND){
+    
+    if(!read_switch(LIMIT_SWITCH)|| (wiper.move_up && millis()- wiper.current_time < 2000))
+      set_wiper_mosfets("EXTEND");
+    
+    else if(wiper.move_up && millis() - wiper.current_time > 2000)
+      wiper.move_up = false;
+    
+    else{
+      set_wiper_mosfets("STOP");
+      wiper.current_time = millis();
+      wiper.state = EXTEND_DELAY;
+    }
+      
+  } else if(wiper.state == RETRACT){
+    
+    if(!read_switch(LIMIT_SWITCH) || (wiper.move_up && millis() - wiper.current_time < 2000))
+      set_wiper_mosfets("RETRACT");
+    
+    else if(wiper.move_up && millis() - wiper.current_time > 2000)
+      wiper.move_up = false;
+    
+    else {
+      set_wiper_mosfets("STOP");
+      wiper.current_time = millis();
+      wiper.state = RETRACT_DELAY;
+    }
+    
+  } else{
+    if(millis() - wiper.current_time > wiper.mosfet_delay){
+      wiper.state = (wiper.state == EXTEND_DELAY) ? RETRACT : EXTEND;
+      wiper.current_time = millis();
+      wiper.move_up = true;
+    } 
+    else
+      set_wiper_mosfets("STOP");
+  }
+}
+
+
+void reset_wiper(WiperController &wiper){
+  if(!read_switch(LIMIT_SWITCH))
+    set_wiper_mosfets("RETRACT");
+  else
+    set_wiper_mosfets("STOP");
+
+  wiper.move_up = false;
+  wiper.state = EXTEND;
+}
+
+
+void set_wiper_mosfets(String direction){  
+  if(direction == "EXTEND"){
+    digitalWrite(IN_1, HIGH);
+    digitalWrite(IN_4, HIGH);
+    digitalWrite(IN_2, LOW);
+    digitalWrite(IN_3, LOW);
+  } else if(direction == "RETRACT"){
+    digitalWrite(IN_2, HIGH);
+    digitalWrite(IN_3, HIGH);
+    digitalWrite(IN_1, LOW);
+    digitalWrite(IN_4, LOW);
+  } else if(direction == "STOP"){
+    digitalWrite(IN_2, LOW);
+    digitalWrite(IN_3, LOW);
+    digitalWrite(IN_1, LOW);
+    digitalWrite(IN_4, LOW);
+  }
 }
--- wiper_tests/wiper_tests.ino
+++ wiper_tests/wiper_tests.ino
@@ -5,101 +5,89 @@
 #define IN_3 11
 #define IN_4 12
 
-
 enum WIPER_STATE {EXTEND, RETRACT, EXTEND_DELAY, RETRACT_DELAY};
+WIPER_STATE wiper_state = EXTEND;
 
-WIPER_STATE wiper_state = RETRACT;
-
-const int wiper_delay = 100;
-
+const int wiper_delay = 2000;
 long unsigned int wiper_time = 0;
+
+bool move_up = false;
 
 bool update_device(int input_port, int output_port);
 bool read_switch(int port);
 
 void setup(){
-  wiper_time = millis();
+  
+  Serial.begin(9600);
+  
   pinMode(IN_1, OUTPUT);
   pinMode(IN_2, OUTPUT);
   pinMode(IN_3, OUTPUT);
   pinMode(IN_4, OUTPUT);
   pinMode(WIPER_SWITCH, INPUT);
-
+  pinMode(LIMIT_SWITCH, INPUT);
+  
+  move_up = false;
+  
+  digitalWrite(IN_1, LOW);
   digitalWrite(IN_2, LOW);
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, LOW);
   delay(50);
+
+  wiper_time = millis();
 }
 
 void loop(){
-
-  /*Serial.println(read_switch(WIPER_SWITCH));
   
   if(read_switch(WIPER_SWITCH)){
-
     if(wiper_state == EXTEND){
-
-      if(!read_switch(LIMIT_SWITCH)){
+      if(!read_switch(LIMIT_SWITCH)|| (move_up && millis()-wiper_time < 2000)){
         set_wipers("EXTEND");
-      } else{
-        disable_wipers();
+      } 
+      else if(move_up && millis()-wiper_time > 2000){
+        move_up = 0;
+      }
+      else{
+        set_wipers("STOP");
         wiper_time = millis();
         wiper_state = EXTEND_DELAY;
       }
       
     } else if(wiper_state == RETRACT){
-
-      if(!read_switch(LIMIT_SWITCH)){
+      if(!read_switch(LIMIT_SWITCH) || (move_up && millis()-wiper_time < 2000)) {
         set_wipers("RETRACT");
-      } else{
-        disable_wipers();
+      } 
+      else if(move_up && millis()-wiper_time > 2000){
+        move_up = 0;
+      }
+      else{
+        set_wipers("STOP");
         wiper_time = millis();
         wiper_state = RETRACT_DELAY;
       }
       
     } else{
-
       if(millis() - wiper_time > wiper_delay){
-        wiper_time = millis();
         wiper_state = (wiper_state == EXTEND_DELAY) ? RETRACT : EXTEND;
+        wiper_time = millis();
+        move_up = 1;
       } else{
-        disable_wipers();
+        set_wipers("STOP");
       }
     }
   } 
-
   // ------ NOT WIPING ------
   else{
-
     if(!read_switch(LIMIT_SWITCH)){
       set_wipers("RETRACT");
     } 
     else
-      disable_wipers();
+      set_wipers("STOP");
  
-    wiper_state = EXTEND;
-  }*/
-
+    wiper_state = EXTEND; 
+  }
   
-  /*Serial.println(read_switch(WIPER_SWITCH));
-  if(read_switch(WIPER_SWITCH)){
-    disable_wipers();
-    delay(50);
-    set_wipers("EXTEND");
-    delay(5000);
-    disable_wipers();
-    delay(50);
-    set_wipers("RETRACT");
-    delay(5000);
-  }
-  else {
-    disable_wipers();
-  }*/
-
-  if(read_switch(WIPER_SWITCH)){
-    digitalWrite(IN_1, HIGH);
-  } else {
-    digitalWrite(IN_1, LOW);
-  }
-
   delay(50);
 }
 
@@ -108,24 +96,25 @@ bool read_switch(int port){
   return digitalRead(port);
 }
 
-
 bool update_device(int input_port, int output_port){
   bool state = read_switch(input_port);
-
   digitalWrite(output_port, state);
   return state;
 }
 
-// direction: 1 is extend, 0 is retract
 void set_wipers(String direction){  
   if(direction == "EXTEND"){
     Serial.println("Extending");
     digitalWrite(IN_1, HIGH);
     digitalWrite(IN_4, HIGH);
+    digitalWrite(IN_2, LOW);
+    digitalWrite(IN_3, LOW);
   } else if(direction == "RETRACT"){
     Serial.println("Retracting");
     digitalWrite(IN_2, HIGH);
     digitalWrite(IN_3, HIGH);
+    digitalWrite(IN_1, LOW);
+    digitalWrite(IN_4, LOW);
   } else if(direction == "STOP"){
     Serial.println("Stopped");
     digitalWrite(IN_2, LOW);
@@ -133,8 +122,4 @@ void set_wipers(String direction){
     digitalWrite(IN_1, LOW);
     digitalWrite(IN_4, LOW);
   }
-}
-
-void disable_wipers(){
-  set_wipers("STOP");
 }
